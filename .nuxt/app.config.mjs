ca66d1ef9a3020b75c11ce3cfe84bfbeb221f46c
Mamuka Anjaparidze <anjaparidzemamuka@gmail.com>
2023-03-31 23:14:24 +0400

    chore: T1 Initialized Vue Project

---
+++ .nuxt/app.config.mjs
@@ -0,0 +1,8 @@
+
+import { defuFn } from '/Users/mamukaanjaparidze/Documents/Coding/Lineate/StaffSync-Front/node_modules/defu/dist/defu.mjs'
+
+const inlineConfig = {}
+
+
+
+export default /* #__PURE__ */ defuFn(inlineConfig)
