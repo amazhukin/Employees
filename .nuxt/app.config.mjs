e2691d9ea2aa1ac29e20507d32053c326ad4371f
AnjaparidzeM <mamuka_anjaparidze@lineate.com>
2023-04-03 20:15:37 +0400

    chore: Remove .Nuxt Folder from Source Control

--- .nuxt/app.config.mjs
+++
@@ -1,8 +0,0 @@
-
-import { defuFn } from '/Users/mamukaanjaparidze/Documents/Coding/Lineate/StaffSync-Front/node_modules/defu/dist/defu.mjs'
-
-const inlineConfig = {}
-
-
-
-export default /* #__PURE__ */ defuFn(inlineConfig)
