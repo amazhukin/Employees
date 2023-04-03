bea83466ad81d0447b330cea8ed41eb6574eb7e8
AnjaparidzeM <mamuka_anjaparidze@lineate.com>
2023-04-03 20:10:30 +0400

    feat: T.3-T.4 Created Routing, Main Page, Employees Page and Components

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <span>Yo</span>
+
+    <!-- TODO: Create General Layout of Onboarding Page -->
+
+
+    <!-- TODO: Create Redirect Button to go to /employees Page -->
+
+
+</template>
