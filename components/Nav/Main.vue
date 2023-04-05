d1e207729870b6a4253c2089d4983cd46982effd
AnjaparidzeM <mamuka_anjaparidze@lineate.com>
2023-04-05 04:53:17 +0400

    feat: T.5-T.6 Researched & Created Nav/Filter Bars in Main Page

--- components/Nav/Main.vue
+++ components/Nav/Main.vue
@@ -1,3 +1,22 @@
 <template>
-    <div>Main NavBar</div>
+    <div class="flex flex-row items-center w-full p-4 bg-gray-100 border-b-[1px] border-gray-300 gap-8">
+        <div class="flex flex-row bg-white p-2 rounded">
+            <img src="#" alt="Search" class="ml-4">
+
+            <input type="text" placeholder="Search" />
+        </div>
+
+        <div class="flex flex-row gap-4">
+            <!-- TODO: Export to Reusable Component -->
+            <div class="hover:bg-blue-300 hover:text-white transition duration-300 cursor-pointer bg-white p-2 rounded">Skills</div>
+            <div class="hover:bg-blue-300 hover:text-white transition duration-300 cursor-pointer bg-white p-2 rounded">Company</div>
+            <div class="hover:bg-blue-300 hover:text-white transition duration-300 cursor-pointer bg-white p-2 rounded">Department</div>
+            <div class="hover:bg-blue-300 hover:text-white transition duration-300 cursor-pointer bg-white p-2 rounded">Position</div>
+            <div class="hover:bg-blue-300 hover:text-white transition duration-300 cursor-pointer bg-white p-2 rounded">Group</div>
+            <div class="hover:bg-blue-300 hover:text-white transition duration-300 cursor-pointer bg-white p-2 rounded">Unit</div>
+            <div class="hover:bg-blue-300 hover:text-white transition duration-300 cursor-pointer bg-white p-2 rounded">People Location</div>
+            <div class="hover:bg-blue-300 hover:text-white transition duration-300 cursor-pointer bg-white p-2 rounded">Country of Office</div>
+            <div class="hover:bg-blue-300 hover:text-white transition duration-300 cursor-pointer bg-white p-2 rounded">Location Country</div>
+        </div>
+    </div>
 </template>
--- components/Nav/Secondary.vue
+++ components/Nav/Secondary.vue
@@ -1,3 +1,16 @@
 <template>
-    <div>Secondary NavBar</div>
+    <div class="flex flex-row items-center w-full p-4 bg-gray-100 border-b-[1px] border-gray-300 gap-8">
+        <div class="flex flex-row bg-white rounded">
+            <div class="cursor-pointer hover:bg-blue-300 hover:text-white transition duration-300 border p-2 text-white bg-blue-400">Any</div>
+            <div class="cursor-pointer hover:bg-blue-300 hover:text-white transition duration-300 border p-2">Working</div>
+            <div class="cursor-pointer hover:bg-blue-300 hover:text-white transition duration-300 border p-2">Away</div>
+        </div>
+
+        <div class="flex flex-row bg-white rounded">
+            <div class="cursor-pointer hover:bg-blue-300 hover:text-white transition duration-300 border p-2">All</div>
+            <div class="cursor-pointer hover:bg-blue-300 hover:text-white transition duration-300 border p-2 text-white bg-blue-400">Employee</div>
+            <div class="cursor-pointer hover:bg-blue-300 hover:text-white transition duration-300 border p-2">Contractor</div>
+            <div class="cursor-pointer hover:bg-blue-300 hover:text-white transition duration-300 border p-2">Subcontractor</div>
+        </div>
+    </div>
 </template>
