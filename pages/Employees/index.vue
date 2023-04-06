388ea9f7ec1bdaee31dd4855cea6123aa2e47652
Mamuka Anjaparidze <anjaparidzemamuka@gmail.com>
2023-04-06 19:38:40 +0400

    feat: Updated Employees Page with EmployeeCards & Data

--- pages/Employees/index.vue
+++ pages/Employees/index.vue
@@ -1,12 +1,52 @@
 <template>
-    <h1>Employees Page</h1>
+  <h1></h1>
 
-    <!-- Upper Filter/Navigation -->
-    <!-- Lower Filter/Navigation -->
+  <SearchBar />
 
-    <Employee />
-    <!-- Employee Cards -->
+  <SortBar />
 
-    <EmployeeCard />
-
+  <div class="flex flex-row flex-wrap justify-center gap-4 w-full mt-8">
+    <EmployeeCard
+      v-for="(employee, x) in dummyData"
+      v-bind:employeeData="employee"
+      :key="x"
+    />
+  </div>
 </template>
+
+<script>
+export default {
+  name: "Employees",
+  data() {
+    return {
+      dummyData: [
+        {
+          id: 0,
+          employee_name: "Avad Mussa",
+          employee_position: "Software Engineer of 2nd Category",
+        },
+        {
+          id: 1,
+          employee_name: "Avad Mussa",
+          employee_position: "Software Engineer of 2nd Category",
+        },
+        {
+          id: 2,
+          employee_name: "Avad Mussa",
+          employee_position: "Software Engineer of 2nd Category",
+        },
+        {
+          id: 3,
+          employee_name: "Avad Mussa",
+          employee_position: "Software Engineer of 2nd Category",
+        },
+        {
+          id: 4,
+          employee_name: "Avad Mussa",
+          employee_position: "Software Engineer of 2nd Category",
+        },
+      ],
+    };
+  },
+};
+</script>
