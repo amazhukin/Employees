9f9aa8eb829003202ba188a24599d2594686f977
MAnjaparidze <anjaparidzemamuka@gmail.com>
2023-04-11 18:32:45 +0400

    feat: T.11 Created Pagination and Data Change Functionalities

---
+++ components/Pagination/index.vue
@@ -0,0 +1,92 @@
+<script setup lang="ts">
+import { ref } from "vue";
+const props = defineProps(["items", "callback"]);
+
+const currentPage = ref(1);
+const pages = ref();
+
+onMounted(() => {
+  handleMapPageNums();
+  handlePagination("FIRST");
+});
+
+watch(currentPage, (newValue, oldValue) => {
+  props.callback(
+    props.items.slice(currentPage.value - 1, currentPage.value + 4)
+  );
+});
+
+const handlePagination = (ACTION_TYPE: string) => {
+  let pagesCount = Math.ceil(props.items.length / 5);
+
+  switch (ACTION_TYPE) {
+    case "FIRST":
+      currentPage.value = 1;
+      break;
+    case "LAST":
+      currentPage.value = pagesCount;
+      break;
+    case "PREV":
+      let prevPage = currentPage.value - 1;
+      currentPage.value = prevPage === 0 ? currentPage.value : prevPage;
+      break;
+    case "NEXT":
+      let nextPage = currentPage.value + 1;
+      currentPage.value = nextPage > pagesCount ? currentPage.value : nextPage;
+      break;
+    default:
+      currentPage.value = 1;
+      break;
+  }
+};
+
+const handleMapPageNums = () => {
+  let count = Math.ceil(props.items.length / 5);
+  let startPage = 1;
+  let paginationLength = count >= 10 ? 10 : count;
+
+  if (currentPage.value >= 6 && currentPage.value + 5 < count) {
+    startPage = currentPage.value - 5;
+  }
+
+  if (currentPage.value + 5 >= count && currentPage.value > 10) {
+    startPage = currentPage.value - 10;
+  }
+  pages.value = Array.from(
+    { length: paginationLength },
+    (_, i) => startPage + i
+  );
+};
+</script>
+
+<template>
+  <div class="flex justify-between mx-auto w-1/2 mt-10 fixed left-1/4 bottom-4">
+    <div>
+      <button class="mr-8" @click="handlePagination('FIRST')">
+        {{ "<<" }}
+      </button>
+
+      <button @click="handlePagination('PREV')">
+        {{ "<" }}
+      </button>
+    </div>
+
+    <div
+      v-for="pageNum in pages"
+      class="cursor-pointer"
+      :class="{ 'text-red-500': pageNum === currentPage }"
+      @click="currentPage = pageNum"
+    >
+      {{ pageNum }}
+    </div>
+
+    <div>
+      <button @click="handlePagination('NEXT')">
+        {{ ">" }}
+      </button>
+      <button class="ml-8" @click="handlePagination('LAST')">
+        {{ ">>" }}
+      </button>
+    </div>
+  </div>
+</template>
--- pages/Employees/index.vue
+++ pages/Employees/index.vue
@@ -3,89 +3,6 @@ import { useEmployeeStore } from "@/store/employeeStore";
 
 const employeeStore = useEmployeeStore();
 
-let dummyData = [
-  {
-    id: 0,
-    employee_image: "https://i.pravatar.cc/64",
-    employee_name: "Michael Johnson",
-    employee_position: "Software Engineer of 2nd Category",
-    employee_salary: 89531,
-    employee_age: 27,
-  },
-  {
-    id: 1,
-    employee_image: "https://i.pravatar.cc/64",
-    employee_name: "Sarah Davis",
-    employee_position: "Software Engineer of 1st Category",
-    employee_salary: 64520,
-    employee_age: 38,
-  },
-  {
-    id: 2,
-    employee_image: "https://i.pravatar.cc/64",
-    employee_name: "John Wilson",
-    employee_position: "Software Engineer of 3rd Category",
-    employee_salary: 22934,
-    employee_age: 47,
-  },
-  {
-    id: 3,
-    employee_image: "https://i.pravatar.cc/64",
-    employee_name: "Sophia Brown",
-    employee_position: "Software Engineer of 1st Category",
-    employee_salary: 37513,
-    employee_age: 42,
-  },
-  {
-    id: 4,
-    employee_image: "https://i.pravatar.cc/64",
-    employee_name: "Alice Taylor",
-    employee_position: "Software Engineer of 2nd Category",
-    employee_salary: 63197,
-    employee_age: 30,
-  },
-  {
-    id: 5,
-    employee_image: "https://i.pravatar.cc/64",
-    employee_name: "Robert Smith",
-    employee_position: "Software Engineer of 3rd Category",
-    employee_salary: 54620,
-    employee_age: 29,
-  },
-  {
-    id: 6,
-    employee_image: "https://i.pravatar.cc/64",
-    employee_name: "Emily Williams",
-    employee_position: "Software Engineer of 1st Category",
-    employee_salary: 77894,
-    employee_age: 23,
-  },
-  {
-    id: 7,
-    employee_image: "https://i.pravatar.cc/64",
-    employee_name: "David Miller",
-    employee_position: "Software Engineer of 2nd Category",
-    employee_salary: 39089,
-    employee_age: 37,
-  },
-  {
-    id: 8,
-    employee_image: "https://i.pravatar.cc/64",
-    employee_name: "Olivia Jones",
-    employee_position: "Software Engineer of 3rd Category",
-    employee_salary: 19481,
-    employee_age: 44,
-  },
-  {
-    id: 9,
-    employee_image: "https://i.pravatar.cc/64",
-    employee_name: "Daniel Moore",
-    employee_position: "Software Engineer of 1st Category",
-    employee_salary: 99616,
-    employee_age: 26,
-  },
-];
-
 const initialEmployee = {
   id: -1,
   employee_image: "",
@@ -95,7 +12,7 @@ const initialEmployee = {
   employee_age: -1,
 };
 
-// const employeeArr = ref(employeeStore.items);
+const paginatedItems = ref([]);
 const employeeModalOpen = ref(false);
 const chosenEmployee = reactive(initialEmployee);
 
@@ -109,6 +26,10 @@ const handleModalClose = () => {
   employeeModalOpen.value = false;
   chosenEmployee.value = initialEmployee;
 };
+
+const handlePaginationChange = (newItems) => {
+  paginatedItems.value = newItems;
+};
 </script>
 
 <template>
@@ -118,12 +39,15 @@ const handleModalClose = () => {
 
   <div class="flex flex-row flex-wrap justify-center gap-4 w-full mt-8">
     <EmployeeCard
-      v-for="(employee, x) in employeeStore.items"
+      v-for="(employee, x) in paginatedItems"
       :employeeData="employee"
       @handleEmployeeClick="handleEmployeeClick"
       :key="x"
     />
   </div>
+
+  <Pagination :items="employeeStore.items" :callback="handlePaginationChange" />
+
   <EmployeeModal
     v-if="employeeModalOpen"
     :employeeData="chosenEmployee.value"
