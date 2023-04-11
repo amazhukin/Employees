d21f4c02fff325cf792a1eca0d6841c8e45702dc
MAnjaparidze <anjaparidzemamuka@gmail.com>
2023-04-11 15:35:54 +0400

    refactor: Remove redundant code

--- store/employeeStore.ts
+++ store/employeeStore.ts
@@ -14,63 +14,6 @@ export interface IEmployeeState {
   items: IEmployee[]
 }
 
-// const state = (): IEmployeeState => ({
-//   items: [],
-// });
-
-// const getters = {
-//   getById: (state: IEmployeeState) => (id: number) => {
-//     return state.items.find(item => item.id === id);
-//   },
-//   getByName: (state: IEmployeeState) => (employee_name: string) => {
-//     return state.items.find(item => item.employee_name.includes(employee_name))
-//   }
-// };
-
-// const actions = {
-//   // async fetchEmployees() {
-//   //   try {
-//   //     const response = await axios.get('https://dummy.restapiexample.com/api/v1/employees');
-//   //     this.items = response.data;
-//   //   } catch (error) {
-//   //     console.error(error);
-//   //   }
-//   // },
-//   // async addEmployee(employee: IEmployee) {
-//   //   try {
-//   //     const response = await axios.post('https://dummy.restapiexample.com/api/v1/create', employee);
-//   //     const state = inject('employeeStore') as IEmployeeState;
-//   //     state.items.push(response.data);
-//   //   } catch (error) {
-//   //     console.error(error);
-//   //   }
-//   // },
-//   // async updateEmployee(employee: IEmployee) {
-//   //   try {
-//   //     const response = await axios.put(`https://dummy.restapiexample.com/api/v1/update/${employee.id}`, employee);
-//   //     const state = inject('employeeStore') as IEmployeeState;
-//   //     const index = state.items.findIndex((item: IEmployee) => item.id === employee.id);
-//   //     if (index !== -1) {
-//   //       state.items.splice(index, 1, response.data);
-//   //     }
-//   //   } catch (error) {
-//   //     console.error(error);
-//   //   }
-//   // },
-//   // async deleteEmployee(id: number) {
-//   //   try {
-//   //     await axios.delete(`https://dummy.restapiexample.com/api/v1/delete/${id}`);
-//   //     const state = inject('employeeStore') as IEmployeeState;
-//   //     const index = state.items.findIndex((item: IEmployee) => item.id === id);
-//   //     if (index !== -1) {
-//   //       state.items.splice(index, 1);
-//   //     }
-//   //   } catch (error) {
-//   //     console.error(error);
-//   //   }
-//   // },
-// };
-
 export const useEmployeeStore = defineStore("employeeStore", {
   state: (): IEmployeeState => ({
     items: [],
@@ -88,6 +31,36 @@ export const useEmployeeStore = defineStore("employeeStore", {
       try {
         const response = await axios.get('https://dummy.restapiexample.com/api/v1/employees');
         this.items = response.data.data;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async addEmployee(employee: IEmployee) {
+      try {
+        const response = await axios.post('https://dummy.restapiexample.com/api/v1/create', employee);
+        this.items.push(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async updateEmployee(employee: IEmployee) {
+      try {
+        const response = await axios.put(`https://dummy.restapiexample.com/api/v1/update/${employee.id}`, employee);
+        const index = this.items.findIndex((item: IEmployee) => item.id === employee.id);
+        if (index !== -1) {
+          this.items.splice(index, 1, response.data);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async deleteEmployee(id: number) {
+      try {
+        await axios.delete(`https://dummy.restapiexample.com/api/v1/delete/${id}`);
+        const index = this.items.findIndex((item: IEmployee) => item.id === id);
+        if (index !== -1) {
+          this.items.splice(index, 1);
+        }
       } catch (error) {
         console.error(error);
       }
